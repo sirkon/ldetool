fcc53b8233a739f9a11f3b4b6e9576495a7b9114
Denis Cheremisov <d.cheremisov@corp.mail.ru>
2017-07-20 23:26:52 +0300

    In process

---
+++ action.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/glossina/ldegen/templatecache"
+	"github.com/urfave/cli"
+)
+
+func generateAction(c *cli.Context) error {
+	path := c.String("code-source")
+	var tc *templatecache.TemplateCache
+	if len(path) != 0 {
+		tc = templatecache.NewFS(path)
+	} else {
+		return fmt.Errorf("Dumped cache usage is not implemented yet, use --codes-source parameter")
+	}
+	if _, err := tc.Get("close_option"); err != nil {
+		return err
+	}
+	return nil
+}
--- ldegen.go
+++ ldegen.go
@@ -2,11 +2,36 @@ package main
 
 //go:generate gocc ldegen_grammar.bnf
 
-import _ "github.com/glossina/ldegen/lexer"
-import _ "github.com/glossina/ldegen/parser"
-import _ "github.com/glossina/ldegen/token"
-import _ "github.com/glossina/ldegen/util"
-import _ "github.com/glossina/ldegen/errors"
+import (
+	"os"
+
+	"github.com/urfave/cli"
+
+	// These are for testing reasons
+	_ "github.com/glossina/ldegen/errors"
+	_ "github.com/glossina/ldegen/lexer"
+	_ "github.com/glossina/ldegen/parser"
+	_ "github.com/glossina/ldegen/token"
+	_ "github.com/glossina/ldegen/util"
+)
 
 func main() {
+	app := cli.NewApp()
+	app.Usage = "Text data extraction Go source code generator"
+	app.UsageText = "ldegen [--code-source <path:>]"
+	app.Commands = []cli.Command{
+		cli.Command{
+			Name:  "generate",
+			Usage: "translate text data extraction rules into source code",
+			Flags: []cli.Flag{
+				cli.StringFlag{
+					Name:  "code-source",
+					Value: "",
+					Usage: "path for templates folder, the template cache will use file system if set",
+				},
+			},
+			Action: generateAction,
+		},
+	}
+	app.Run(os.Args)
 }
