ee59cfdb86c5cc12d712d9b79557c33485f9d7f4
izgib <sasha.cheremisov@gmail.com>
2017-09-08 14:10:49 +0300

    Lookup options

--- ast/targets.go
+++ ast/targets.go
@@ -12,6 +12,7 @@ type Target struct {
 	Value string
 	Lower int
 	Upper int
+	Close bool
 }
 
 // BoundedScopeStringTarget ...
@@ -58,12 +59,39 @@ func LimitedScopeStringTarget(needle Attrib, limitation Attrib) (attr Attrib, er
 	return res, nil
 }
 
+// FixedStringTarget ...
+func FixedStringTarget(needle Attrib, limitation Attrib) (attr Attrib, err error) {
+	n := needle.(*token.Token)
+	l := limitation.(*token.Token)
+	limit, err := strconv.ParseUint(string(l.Lit), 10, 64)
+	if err != nil || limit == 0 {
+		err = TokenError(l)("bad number %s", string(l.Lit))
+		return
+	}
+	res := Target{
+		Type:  String,
+		Value: string(n.Lit),
+		Lower: int(limit),
+		Upper: int(limit),
+	}
+	return res, nil
+}
+
+// CloseStringTarget ...
+func CloseStringTarget(needle Attrib) (attr Attrib, err error) {
+	res := Target{
+		Type:  String,
+		Value: string(needle.(*token.Token).Lit),
+		Close: true,
+	}
+	return res, nil
+}
+
 // StringTarget ...
 func StringTarget(needle Attrib) (attr Attrib, err error) {
 	res := Target{
 		Type:  String,
 		Value: string(needle.(*token.Token).Lit),
-		Upper: 0,
 	}
 	return res, nil
 }
@@ -108,6 +136,34 @@ func LimitedScopeCharTarget(needle Attrib, limitation Attrib) (attr Attrib, err 
 		Type:  Char,
 		Value: string(n.Lit),
 		Upper: int(limit),
+	}
+	return res, nil
+}
+
+// FixedCharTarget ...
+func FixedCharTarget(needle Attrib, limitation Attrib) (attr Attrib, err error) {
+	n := needle.(*token.Token)
+	l := limitation.(*token.Token)
+	limit, err := strconv.ParseUint(string(l.Lit), 10, 64)
+	if err != nil || limit == 0 {
+		err = TokenError(l)("bad number %s", string(l.Lit))
+		return
+	}
+	res := Target{
+		Type:  Char,
+		Value: string(n.Lit),
+		Lower: int(limit),
+		Upper: int(limit),
+	}
+	return res, nil
+}
+
+// CloseCharTarget ...
+func CloseCharTarget(needle Attrib) (attr Attrib, err error) {
+	res := Target{
+		Type:  Char,
+		Value: string(needle.(*token.Token).Lit),
+		Close: true,
 	}
 	return res, nil
 }
