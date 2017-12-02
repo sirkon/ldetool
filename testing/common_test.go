794070b39ddb0d6609872fecff8f1d7b920ceb43
Denis Cheremisov <d.cheremisov@corp.mail.ru>
2017-12-02 17:47:21 +0300

    missed files

---
+++ testing/common_test.go
@@ -0,0 +1,21 @@
+package ldetesting
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestCommon(t *testing.T) {
+	data := `[bugaga] -123  234`
+	e := &Rule{}
+	if ok, err := e.Extract(data); !ok || err != nil {
+		if err != nil {
+			t.Fatal(err)
+		}
+		require.True(t, ok)
+	}
+	require.Equal(t, "bugaga", e.Data)
+	require.Equal(t, -123, e.Signed)
+	require.Equal(t, uint(234), e.Unsigned)
+}
