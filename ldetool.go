cf65cf7141f96fab800e430beb50b7b27c083f47
Denis Cheremisov <denis.cheremisov@gmail.com>
2018-08-25 12:21:32 +0300

    proper destination for antlr

--- ldetool.go
+++ ldetool.go
@@ -1,6 +1,6 @@
 package main
 
-//go:generate antlr4 -visitor -no-visitor -listener -o parser -Dlanguage=Go LDE.g4
+//go:generate antlr4 -visitor -no-visitor -listener -o internal/parser -Dlanguage=Go LDE.g4
 
 import (
 	"os"
