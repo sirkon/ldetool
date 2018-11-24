0751c2476c782f29b8e6a7d70c7074ff155c0aba
Denis Cheremisov <denis.cheremisov@gmail.com>
2018-11-24 14:24:33 +0300

    feature 5 done

--- ldetool.go
+++ ldetool.go
@@ -3,7 +3,12 @@ package main
 //go:generate antlr4 -visitor -no-visitor -listener -o internal/parser -Dlanguage=Go LDE.g4
 
 import (
+	"go/parser"
+	"go/token"
+	"io/ioutil"
 	"os"
+	"path"
+	"strings"
 
 	"github.com/urfave/cli"
 
@@ -16,17 +21,11 @@ import (
 func main() {
 	app := cli.NewApp()
 	app.Usage = "Text data extraction Go source code generator"
-	app.UsageText = "ldetool [--code-source <path:>]"
 	app.Commands = []cli.Command{
 		{
 			Name:  "generate",
 			Usage: "translate text data extraction rules into source code",
 			Flags: []cli.Flag{
-				cli.StringFlag{
-					Name:  "code-source",
-					Value: "",
-					Usage: "path for templates folder, template cache will use file system if set",
-				},
 				cli.StringFlag{
 					Name:  "yaml-dict",
 					Value: "",
@@ -61,10 +60,12 @@ func main() {
 				if c.NArg() != 1 {
 					return cli.NewExitError("There must be one and only one lde rule file", 1)
 				}
+				c.Args().First()
 				if len(c.String("yaml-dict")) > 0 && len(c.String("json-dict")) > 0 {
 					return cli.NewExitError(
 						"Cannot use yaml and json formatting dictionaries at the same time, choose one of them", 1)
 				}
+				resolvePackageName(c)
 				if len(c.String("package")) == 0 {
 					return cli.NewExitError("Package name is required", 1)
 				}
@@ -77,5 +78,50 @@ func main() {
 	}
 	if err := app.Run(os.Args); err != nil {
 		message.Critical(err)
+	}
+}
+
+func resolvePackageName(c *cli.Context) {
+	outputFileName := getOutputFileName(c.Args().First())
+	dirPath, outFileBaseName := path.Split(outputFileName)
+	if len(dirPath) == 0 {
+		dirPath = "."
+	}
+	files, err := ioutil.ReadDir(dirPath)
+	if err != nil {
+		message.Fatalf("failed to read `%s`: %s", dirPath, err)
+	}
+
+	var packageDetected bool
+	for _, file := range files {
+		if strings.HasSuffix(file.Name(), ".go") || file.Name() != outFileBaseName {
+			packageDetected = true
+			break
+		}
+	}
+	if !packageDetected {
+		return
+	}
+
+	// parse package to extract package name
+	fset := token.NewFileSet()
+	pkgs, err := parser.ParseDir(fset, dirPath, func(info os.FileInfo) bool {
+		return info.Name() != outFileBaseName
+	}, parser.PackageClauseOnly)
+	if err != nil {
+		message.Warningf("failed to parse one of Go source files, thus cannot resolve package name: %s", err)
+		return
+	}
+	if len(pkgs) > 1 {
+		message.Warningf("located %d packages in `%s`, thus cannot set up a package name", len(pkgs), dirPath)
+	}
+	if len(pkgs) == 0 {
+		return
+	}
+	for pkgName := range pkgs {
+		if err := c.Set("package", pkgName); err != nil {
+			message.Warningf("failed to set up package name: %s", err)
+		}
+		return
 	}
 }
